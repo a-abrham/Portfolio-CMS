07867ba3ce89b8d697bcd36a258d274d593508ae
abrshkorra <abrshkorra@gmail.com>
2024-04-02 11:25:20 +0300

    entry

---
+++ portfolio-website-content-managment-system/schemaTypes/index.ts
@@ -0,0 +1,6 @@
+import {projects} from './projects'
+import {blogs} from './blogs'
+import {progLang} from './progLang'
+import {edu} from './edu'
+import {exp} from './exp'
+export const schemaTypes = [blogs, projects, progLang, edu, exp]
